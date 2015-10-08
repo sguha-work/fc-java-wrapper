3277d11ba4abb89158d7aafd5ba3d64bfc550d6f
Subhasis Ghosal <subhasis@fusioncharts.com>
2015-10-08 11:19:33 +0530

    Update and rename Fusioncharts.java to FusionCharts.java

--- FusionCharts.java
+++ FusionCharts.java
@@ -2,12 +2,12 @@
  *
  * @Contributors Sahasrangshu Guha, Subhasis Ghosal
  */
-public class Fusioncharts {
+public class FusionCharts {
     private String constructorTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {new FusionCharts(__constructorOptions__);});</script>";
     private String renderTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {                FusionCharts(\"__chartId__\").render();});</script>";
     private String[] chartOptions = new String[10];
     private String chartDataSource = "";
-    public Fusioncharts(String type, String id, int width, int height, String renderAt, String dataFormat, String dataSource) {
+    public FusionCharts(String type, String id, int width, int height, String renderAt, String dataFormat, String dataSource) {
         this.chartOptions[0] = id;
         this.chartOptions[1] = Integer.toString(width);
         this.chartOptions[2] = Integer.toString(height);
